769f194bd22e9e5d53d96f831400565f23ab9cde
Nicole Sanders <nsanders9022@gmail.com>
2017-03-03 10:38:11 -0800

    add forms to the bands.cshtml and venues.cshtml pags to add a new instance. Add post methods

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -15,12 +15,30 @@ namespace BandTracker
 
             Get["/bands"] = _ => {
                 List<Band> allBands = Band.GetAll();
-                return View["bands.cshtml"];
+                return View["bands.cshtml", allBands];
             };
 
             Get["/venues"] = _ => {
                 List<Venue> allVenues = Venue.GetAll();
-                return View["venues.cshtml"];
+                return View["venues.cshtml", allVenues];
+            };
+
+            // Get["/band/{id}"] = parameters => {
+            //
+            // };
+
+            Post["/bands/new"] = _ => {
+                Band newBand = new Band(Request.Form["band-name"]);
+                newBand.Save();
+                List<Band> allBands = Band.GetAll();
+                return View["bands.cshtml", allBands];
+            };
+
+            Post["/venues/new"] = _ => {
+                Venue newVenue = new Venue(Request.Form["venue-name"]);
+                newVenue.Save();
+                List<Venue> allVenues = Venue.GetAll();
+                return View["venues.cshtml", allVenues];
             };
         }
     }
